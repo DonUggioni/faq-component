29397a9a4b7c2f09a885650f457ec4ee0bf49de0
Renan Tiscoski <99286931+DonUggioni@users.noreply.github.com>
2022-05-07 18:54:31 +0100

    Update files
    Fixed issue with image on mobile displays. Added JavaScript to perform that action.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@
 const questionEl = document.querySelectorAll('.question');
 const answerEl = document.querySelectorAll('.answer');
 const iconEl = document.querySelectorAll('.icon');
+const illustrationDesktop = document.querySelector('.illustration-desktop');
+const illustrationMobile = document.querySelector('.illustration-mobile');
 
 function removeClass() {
   for (let i = 0; i < questionEl.length; i++) {
@@ -24,3 +26,13 @@ for (let i = 0; i < questionEl.length; i++) {
     iconEl[i].classList.toggle('icon--reverse');
   });
 }
+
+window.addEventListener('resize', function () {
+  if (this.window.innerWidth <= 864) {
+    illustrationDesktop.classList.add('illustration--invisible');
+    illustrationMobile.classList.remove('illustration--invisible');
+  } else {
+    illustrationDesktop.classList.remove('illustration--invisible');
+    illustrationMobile.classList.add('illustration--invisible');
+  }
+});
